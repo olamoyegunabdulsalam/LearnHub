babec5a100df2900a693656f25ec6df10839e8b1
Olamoyegun Abdulsalam <olamoyegunabdulsalam59@gmail.com>
2025-11-17 20:46:39 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function Header({ onProfileClick }) {
   return (
-    <header className="sticky z-20  bg-purple-600 text-white p-4 flex justify-between items-center shadow-md">
+    <header className="sticky top-0 z-20  bg-purple-600 text-white p-4 flex justify-between items-center shadow-md">
       <h1 className="text-2xl font-bold">
         <i class="fa-solid fa-book-open"></i>LearnHub
       </h1>
